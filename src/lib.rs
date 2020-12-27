0d9abdceec2a999986caeddfb3c158e347b95198
tpltnt <1172976+tpltnt@users.noreply.github.com>
2020-12-27 22:40:00 +0100

    added cell retrieval

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! `roux` provides data-structures and functions
 //! to implement Cellular Automata.
-#![no_std]
+//#![no_std]
 
 /// The state of a cell.
 #[derive(Debug, PartialEq)]
@@ -11,17 +11,17 @@ enum CellState {
 
 /// A cell on a grid.
 /// Cell positions start at top left corner.
-struct Cell<'a> {
+struct Cell {
     state: CellState,
     horizontal_position: u8, // for more adjust data types
     vertical_position: u8,   // -> see also: Grid
-    prev: *mut Cell<'a>,
-    next: *mut Cell<'a>,
+    prev: *mut Cell,
+    next: *mut Cell,
 }
 
-impl Cell<'_> {
+impl Cell {
     /// Create a new dead cell at top-left position.
-    pub fn new() -> Cell<'static> {
+    pub fn new() -> Cell {
         Cell {
             state: CellState::Dead,
             horizontal_position: 0,
@@ -33,15 +33,15 @@ impl Cell<'_> {
 }
 
 /// A structure to encode a grid with cells.
-struct Grid<'a> {
+struct Grid {
     // size allows for 256x256 cells -> enough for embedded
     // -> for more adjust the data types
     horizontal_size: u8,
     vertical_size: u8,
-    start: *mut Cell<'a>,
+    start: *mut Cell,
 }
 
-impl Grid<'_> {
+impl Grid {
     /// Create a new grid with the given dimensions and
     /// fill it with default (dead) cells.
     ///
@@ -55,8 +55,8 @@ impl Grid<'_> {
     /// grid. 256x256 are currently enough cells for embedded applications.
     /// Larger grid sizes have to keep the target usize (thus architecture)
     /// in mind and can be adjusted appropriately.
-    pub fn new(h_size: u8, v_size: u8) -> Grid<'static> {
-        let mut lastcell: *mut Cell<'_> = core::ptr::null_mut();
+    pub fn new(h_size: u8, v_size: u8) -> Grid {
+        let mut lastcell: *mut Cell = core::ptr::null_mut();
 
         // create/allocate all cells as linked list
         // (in reverse order)
@@ -66,7 +66,7 @@ impl Grid<'_> {
                 c.horizontal_position = h;
                 c.vertical_position = v;
                 c.prev = lastcell;
-                lastcell = &mut c as *mut Cell<'_>;
+                lastcell = &mut c as *mut Cell;
             }
         }
 
@@ -90,6 +90,36 @@ impl Grid<'_> {
             start: lastcell,
         }
     }
+
+    /// Retrieve a cell (for modification).
+    ///
+    /// # Arguments
+    /// * `h`: horizontal coordinate
+    /// * `v`: vertical coordinate
+    pub fn get_cell(&self, h: u8, v: u8) -> &Cell {
+        let mut tmp = &self.start;
+        let mut watchdog: usize = 0;
+
+        if h >= self.horizontal_size {
+            panic!("horizontal coordinate too large")
+        }
+        if v >= self.vertical_size {
+            panic!("vertical coordinate too large")
+        }
+
+        loop {
+            unsafe {
+                if (*(*tmp)).horizontal_position == h && (*(*tmp)).vertical_position == v {
+                    break &(*(*tmp));
+                }
+                tmp = &(*(*tmp)).next;
+            }
+            if watchdog > (h * v) as usize {
+                panic!("endless loop")
+            }
+            watchdog += 1;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -111,5 +141,30 @@ mod tests {
         let g = Grid::new(5, 23);
         assert_eq!(g.horizontal_size, 5);
         assert_eq!(g.vertical_size, 23);
+    }
+
+    //#[test]
+    // check grid creation values
+    fn grid_get_cell() {
+        let g = Grid::new(3, 17);
+        assert_eq!(g.horizontal_size, 3);
+        assert_eq!(g.vertical_size, 17);
+        let c = g.get_cell(1, 8);
+        assert_eq!(c.horizontal_position, 1);
+        assert_eq!(c.vertical_position, 8);
+    }
+
+    #[test]
+    #[should_panic]
+    fn grid_get_cell_v_too_large() {
+        let g = Grid::new(3, 17);
+        let _c = g.get_cell(1, 17);
+    }
+
+    //#[test]
+    //#[should_panic]
+    fn grid_get_cell_h_too_large() {
+        let g = Grid::new(3, 1);
+        let _c = g.get_cell(3, 0);
     }
 }
