639b99e5df1133a11348bd50eb4566cf367edddd
tpltnt <1172976+tpltnt@users.noreply.github.com>
2020-12-27 02:21:09 +0100

    implemented basic cell

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,41 @@
 //! to implement Cellular Automata.
 
 /// The state of a cell.
+#[derive(Debug, PartialEq)]
 enum CellState {
     Dead,
     Alive,
 }
 
+/// A cell on a grid.
+/// Cell positions start at top left corner.
+struct Cell {
+    state: CellState,
+    horizontal_position: usize,
+    vertical_position: usize,
+}
+
+impl Cell {
+    /// Create a new dead cell at top-left position.
+    pub fn new() -> Cell {
+        Cell {
+            state: CellState::Dead,
+            horizontal_position: 0,
+            vertical_position: 0,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    // check default values
+    fn cell_new() {
+        let c = Cell::new();
+        assert_eq!(c.state, CellState::Dead);
+        assert_eq!(c.horizontal_position, 0);
+        assert_eq!(c.vertical_position, 0);
     }
 }
