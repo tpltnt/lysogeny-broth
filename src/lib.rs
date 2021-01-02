7f89f376c494930f0639d464e995b4a0a18cb184
tpltnt <1172976+tpltnt@users.noreply.github.com>
2021-01-02 15:17:06 +0100

    removed struct Cell

--- src/lib.rs
+++ src/lib.rs
@@ -8,42 +8,20 @@ const HEAP_SIZE: usize = 10 * 1024; // 10KB
 const LEAF_SIZE: usize = 16;
 
 /// The state of a cell.
-#[derive(Debug, PartialEq)]
-enum CellState {
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum CellState {
     Dead,
     Alive,
 }
 
-/// A cell on a grid.
-/// Cell positions start at top left corner.
-pub struct Cell {
-    state: CellState,
-    horizontal_position: u8, // for more adjust data types
-    vertical_position: u8,   // -> see also: Grid
-    prev: *mut Cell,
-    next: *mut Cell,
-}
-
-impl Cell {
-    /// Create a new dead cell at top-left position.
-    pub fn new() -> Cell {
-        Cell {
-            state: CellState::Dead,
-            horizontal_position: 0,
-            vertical_position: 0,
-            prev: core::ptr::null_mut(),
-            next: core::ptr::null_mut(),
-        }
-    }
-}
-
 /// A structure to encode a grid with cells.
+/// Cell positions start at top left corner.
 pub struct Grid {
     // size allows for 256x256 cells -> enough for embedded
     // -> for more adjust the data types
     horizontal_size: u8,
     vertical_size: u8,
-    start: *mut Cell,
+    cells: Vec<CellState>,
 }
 
 impl Grid {
@@ -61,53 +39,23 @@ impl Grid {
     /// Larger grid sizes have to keep the target usize (thus architecture)
     /// in mind and can be adjusted appropriately.
     pub fn new(h_size: u8, v_size: u8) -> Grid {
-        let mut lastcell: *mut Cell = core::ptr::null_mut();
-        let mut watchdog: usize = 0;
-
-        // create/allocate all cells as linked list
-        // (in reverse order)
-        for h in 0..h_size {
-            for v in 0..v_size {
-                let mut c = Cell::new();
-                c.horizontal_position = h;
-                c.vertical_position = v;
-                c.prev = lastcell;
-                lastcell = &mut c as *mut Cell;
-            }
-        }
-
-        // link them the other way
-        loop {
-            unsafe {
-                // first cell allocated, prev ptr is NULL -> stop & quit
-                if (*lastcell).horizontal_position != 0 && (*lastcell).vertical_position != 0 {
-                    break;
-                }
-                (*(*lastcell).prev).next = lastcell; // the previous cell has the current cell as next one
-                lastcell = (*lastcell).prev; // go back one cell & repeat
-            }
-            if watchdog > (h_size * v_size) as usize {
-                panic!("endless loop")
-            }
-            watchdog += 1;
-        }
+        let mut all_cells = vec![CellState::Dead; (h_size * v_size) as usize];
 
         // lastcell now points to first element in list
         // -> add to grid as starting point
         Grid {
             horizontal_size: h_size,
             vertical_size: v_size,
-            start: lastcell,
+            cells: vec![],
         }
     }
 
-    /// Retrieve a cell (for modification).
+    /// Retrieve a cell state (for modification).
     ///
     /// # Arguments
     /// * `h`: horizontal coordinate
     /// * `v`: vertical coordinate
-    pub fn get_cell(&self, h: u8, v: u8) -> &Cell {
-        let mut tmp = &self.start;
+    pub fn get_cellstate(&self, h: u8, v: u8) -> CellState {
         let mut watchdog: usize = 0;
 
         if h >= self.horizontal_size {
@@ -116,34 +64,13 @@ impl Grid {
         if v >= self.vertical_size {
             panic!("vertical coordinate too large")
         }
-
-        loop {
-            unsafe {
-                if (*(*tmp)).horizontal_position == h && (*(*tmp)).vertical_position == v {
-                    break &(*(*tmp));
-                }
-                tmp = &(*(*tmp)).next;
-            }
-            if watchdog > (h * v) as usize {
-                panic!("endless loop")
-            }
-            watchdog += 1;
-        }
+        return self.cells[4];
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    // check default values
-    fn cell_new() {
-        let c = Cell::new();
-        assert_eq!(c.state, CellState::Dead);
-        assert_eq!(c.horizontal_position, 0);
-        assert_eq!(c.vertical_position, 0);
-    }
 
     #[test]
     // check grid creation values
